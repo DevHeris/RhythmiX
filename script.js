4a40b9ece66079b438e1fad1f07ba4a39e727db4
DevHeris <badairoinioluwa578@gmail.com>
2023-10-31 22:50:09 +0200

    styled the header and the made the grid area

---
+++ script.js
@@ -0,0 +1,18 @@
+// const header = document.getElementById("bottom-sidebar-item-header");
+// let hasScrolled = false;
+
+// // Function to add shadow to header on scroll
+// function handleScroll() {
+//   if (window.scrollY > 0) {
+//     header.classList.add("header-shadow");
+//     hasScrolled = true;
+//     window.removeEventListener("scroll", handleScroll); // Remove the scroll event listener once shadow is applied
+//   }
+// }
+
+// // Event listener to listen for scroll and trigger handleScroll function
+// window.addEventListener("scroll", () => {
+//   if (!hasScrolled) {
+//     handleScroll();
+//   }
+// });
