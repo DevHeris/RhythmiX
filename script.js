698ce3c4943e71796be92d413fa46156b45c401e
DevHeris <badairoinioluwa578@gmail.com>
2023-11-02 06:23:31 +0200

    more styling in the hero section

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const initSwiper = (swiper) => {
-  const swiper = new Swiper(swiper, {
+function displayRecents() {
+  const swiper = new Swiper(".swiper-1", {
     slidesPerView: 4,
     spaceBetween: 30,
     freeMode: true,
@@ -9,7 +9,20 @@ const initSwiper = (swiper) => {
       disableOnInteraction: false,
     },
   });
-};
+}
 
-initSwiper(".swiper");
-initSwiper(".swiper-1");
+function displayFaves() {
+  const swiper = new Swiper(".swiper-2", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 4500,
+      disableOnInteraction: false,
+    },
+  });
+}
+
+displayRecents();
+displayFaves();
