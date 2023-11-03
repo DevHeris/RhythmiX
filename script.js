8c9b554128e7bb45d4023e9771b1bbb772530761
DevHeris <badairoinioluwa578@gmail.com>
2023-11-03 17:10:29 +0200

    Cleaned up the HTML and CSS code

--- script.js
+++ script.js
@@ -26,3 +26,30 @@ function displayFaves() {
 
 displayRecents();
 displayFaves();
+
+const client_id = "22a4c87ecf6b453dada8e588978d033d";
+const redirect_uri = "http://localhost:5500/index.html";
+const scope = "user-read-private user-read-email";
+
+let url = "https://accounts.spotify.com/authorize";
+url += "?response_type=token";
+url += "&client_id=" + encodeURIComponent(client_id);
+url += "&scope=" + encodeURIComponent(scope);
+url += "&redirect_uri=" + encodeURIComponent(redirect_uri);
+
+console.log(url);
+const access_token = window.location.search;
+console.log(access_token);
+// async function fetchData() {
+//   const result = await fetch(`https://api.spotify.com/v1/tracks/${client_id}`, {
+//     method: "Get",
+//     headers: {
+//       "Content-Type": "application.json",
+//       authorization: "",
+//     },
+//   });
+
+//   console.log(result);
+// }
+
+// fetchData();
